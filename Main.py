a457b4057abec7a1db74c805c87c16466fbac855
Juan Esteban Londoño T <juanestebanlt@gmail.com>
2017-11-09 22:39:49 -0500

    se quita el login por problemas con el decrypt de la informacion del usuario

--- Main.py
+++ Main.py
@@ -1,13 +1,15 @@
 import sys
 
+from Descargas import FrmDescargas
 from Login import Login
 from ui.Dashboard import *
 
 __author__ = "Juan Esteban Londoño Tabares"
 
-
 if __name__ == "__main__":
     app_music = QtGui.QApplication(sys.argv)
-    frm_login = Login()
-    frm_login.show()
+    # frm_login = Login()
+    # frm_login.show()
+    frm_descargas = FrmDescargas()
+    frm_descargas.show()
     sys.exit(app_music.exec_())
